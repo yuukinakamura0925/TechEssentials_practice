309c6d43b98ad97315f879ef951e8fc7a75daf1f
yuukinakamura0925 <ynakamara04@gmail.com>
2021-03-11 22:58:10 +0900

    [edit]fizzbuzz.rb
    fizzbuzz.rbのif文内容を編集

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -2,11 +2,11 @@ print "出力したい上限の数を入力してください >"
 limit_number = gets.to_i
 
 for num in 1..limit_number
-  if num % 3 == 0
+  if num % 15 == 0
     puts "Fizz"
-  elsif num % 5 == 0
+  elsif num % 3 == 0
     puts "Buzz"
-  elsif num % 15 == 0
+  elsif num % 5 == 0
     puts "FizzBuzz"
   else
     puts num
